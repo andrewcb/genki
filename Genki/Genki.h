a0e224d234dc5a44e21cce4ed24c20f805c9b15f
acb <acb@staticplain.local>
2018-06-30 16:15:42 +0100

    Initial Commit

---
+++ Genki/Genki.h
@@ -0,0 +1,19 @@
+//
+//  Genki.h
+//  Genki
+//
+//  Created by acb on 30/06/2018.
+//  Copyright © 2018 Kineticfactory. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Genki.
+FOUNDATION_EXPORT double GenkiVersionNumber;
+
+//! Project version string for Genki.
+FOUNDATION_EXPORT const unsigned char GenkiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Genki/PublicHeader.h>
+
+
